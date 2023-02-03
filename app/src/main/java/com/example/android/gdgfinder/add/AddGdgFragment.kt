78e5597de4283ad14f7398660f15b222e3217dff
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-02-02 23:52:22 -0500

    Adding code from section "3.5.24. (3:04) Exercise: Add a Live Region"

--- app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/add/AddGdgFragment.kt
@@ -1,13 +1,11 @@
 package com.example.android.gdgfinder.add
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.gdgfinder.R
 import com.example.android.gdgfinder.databinding.AddGdgFragmentBinding
 import com.google.android.material.snackbar.Snackbar
@@ -36,6 +34,9 @@ class AddGdgFragment : Fragment() {
 					Snackbar.LENGTH_SHORT // How long to display the message.
 				).show()
 				viewModel.doneShowingSnackbar()
+
+				binding.button.contentDescription = getString(R.string.submitted)
+				binding.button.text = getString(R.string.done)
 			}
 		}
 		setHasOptionsMenu(true)
